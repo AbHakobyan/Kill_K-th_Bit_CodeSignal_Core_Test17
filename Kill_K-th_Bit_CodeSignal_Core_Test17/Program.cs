06d5b925e965bbdd9fed6ece24fa134856572273
Albert Hakobyan <abhakobyan24@gmail.com>
2021-08-24 12:21:56 +0400

    Добавьте файлы проекта.

---
+++ Kill_K-th_Bit_CodeSignal_Core_Test17/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kill_K_th_Bit_CodeSignal_Core_Test17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input n given number");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input k bit from the righ");
+            int k = int.Parse(Console.ReadLine());
+            int item = killKthBit(n, k);
+            Console.WriteLine(item);
+        }
+
+        static int killKthBit(int n, int k)
+        {
+            return n & ~(1 << (k - 1));
+        }
+
+    }
+}
